a8290d317ddc663fcea9a9e107d338311cc17dbb
Israel Lopez <iblopez@ksu.edu>
2020-02-05 14:16:10 -0600

    Class-Work

--- DoesItBlend/CellPhone.cs
+++ DoesItBlend/CellPhone.cs
@@ -8,7 +8,7 @@ namespace DoesItBlend
     /// A class representing a cell phone
     /// </summary>
     /// <summary>
-    public class CellPhone
+    public class CellPhone:IBlendable//, ICloneable, ICloneable
     {
         /// Blends the ice cubes
         /// </summary>
